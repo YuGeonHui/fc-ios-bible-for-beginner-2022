1f1e08a5619f5ea90764fc2d1079d9a22e5ba078
YuGeonHui <ghyu_0906@naver.com>
2022-06-09 21:52:13 +0900

    DataSource -> Diffable DataSource

--- Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_01-AppleFramework_DiffableDatasource/starter/06_01-AppleFramework_DiffableDatasource/AppleFramework/FrameworkListViewController.swift
+++ Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_01-AppleFramework_DiffableDatasource/starter/06_01-AppleFramework_DiffableDatasource/AppleFramework/FrameworkListViewController.swift
@@ -12,74 +12,27 @@ class FrameworkListViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     let list: [AppleFramework] = AppleFramework.list
+
+//    var dataSource: UICollectionViewDiffableDataSource<Section, AppleFramework>!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    typealias Item = AppleFramework
+    enum Section {
+        case main
+    }
     
     // Data, Presentation, Layout
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.dataSource = self
         collectionView.delegate = self
-        
         navigationController?.navigationBar.topItem?.title = "☀️ Apple Frameworks"
         
-        if let flowlayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            flowlayout.estimatedItemSize = .zero
-        }
-        
-        collectionView.contentInset = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
-    }
-}
-
-extension FrameworkListViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return list.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
-            return UICollectionViewCell()
-        }
-        let framework = list[indexPath.item]
-        cell.configure(framework)
-        return cell
-    }
-}
-
-extension FrameworkListViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        // 3열일때 계산
-        let interItemSpacing: CGFloat = 10
-        let padding: CGFloat = 16
-
-        let width = (collectionView.bounds.width - interItemSpacing * 2 - padding * 2) / 3
-        let height = width * 1.5
-        return CGSize(width: width, height: height)
-        
-        
-//        // 2열일때 계산
-//        let interItemSpacing: CGFloat = 10
-//        let padding: CGFloat = 16
-//
-//        let width = (collectionView.bounds.width - interItemSpacing * 1 - padding * 2) / 2
-//        let height = width * 1.5
-//        return CGSize(width: width, height: height)
-//
-//        // 4열일때 계산
-//        let interItemSpacing: CGFloat = 10
-//        let padding: CGFloat = 16
-//
-//        let width = (collectionView.bounds.width - interItemSpacing * 3 - padding * 2) / 4
-//        let height = width * 1.5
-//        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
+        // Presentation, Data, Layout
+//        diffable datasoure : Presentation
+//        snapshot : Data
+//        compositional Layout : Layout
+      
     }
 }
 
