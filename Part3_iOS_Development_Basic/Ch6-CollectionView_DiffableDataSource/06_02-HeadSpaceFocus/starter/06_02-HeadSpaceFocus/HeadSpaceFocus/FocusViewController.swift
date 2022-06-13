5227057f456900948a31f8c5ff2f8beedfcecb21
YuGeonHui <ghyu_0906@naver.com>
2022-06-13 22:45:47 +0900

    Finish Project

--- Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_02-HeadSpaceFocus/starter/06_02-HeadSpaceFocus/HeadSpaceFocus/FocusViewController.swift
+++ Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_02-HeadSpaceFocus/starter/06_02-HeadSpaceFocus/HeadSpaceFocus/FocusViewController.swift
@@ -9,8 +9,87 @@ import UIKit
 
 class FocusViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    var curted: Bool = false
+    
+    var items: [Focus] = Focus.list
+    
+    typealias Item = Focus
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        refreshButton.layer.cornerRadius = 10
+        
+        // Presentation, Data, Layout
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FocusCell", for: indexPath) as? FocusCell else {
+                return nil
+            }
+            
+//            let data = self.items[indexPath.item]
+//            cell.configure(data)
+            
+            cell.configure(itemIdentifier)
+            return cell
+        })
+        
+        // Sanpshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        // Layout (section -> group -> item)
+        collectionView.collectionViewLayout = layout()
+        
+        let title = curted ? "See All" : "See Recommedation"
+        refreshButton.setTitle(title, for: .normal)
+        
+        self.updateButtonTitle()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+    
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        section.interGroupSpacing = 10
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    @IBAction func refreshButtonTapped(_ sender: Any) {
+        
+        curted.toggle()
+        
+        self.items = curted ? Focus.recommendations : Focus.list
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        self.updateButtonTitle()
+    }
+    
+    private func updateButtonTitle() {
+        let title = curted ? "See All" : "See Recommedation"
+        refreshButton.setTitle(title, for: .normal)
     }
 }
