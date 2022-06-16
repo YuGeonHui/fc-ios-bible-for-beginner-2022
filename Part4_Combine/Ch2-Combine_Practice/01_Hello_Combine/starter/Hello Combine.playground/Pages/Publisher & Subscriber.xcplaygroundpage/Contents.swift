8e6ed8b22ee2e464d2130737f4c25d2a7708ad41
YuGeonHui <ghyu_0906@naver.com>
2022-06-16 21:45:16 +0900

    Just sink assign

--- Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
+++ Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
@@ -4,15 +4,29 @@ import Foundation
 import Combine
 
 // Publisher & Subscriber
+let just = Just(1000)
+let subscription1 = just.sink { value in
+    print("Received Value : \(value)")
+}
 
+let arrayPublisher = [1, 3, 5, 7, 9].publisher
+let subscription2 = arrayPublisher.sink { value in
+    print("Received Value : \(value)")
+}
 
+class MyClass {
+    
+    var property: Int = 0 {
+        didSet {
+            print("Did set property to \(property)")
+        }
+    }
+}
 
-
-
-
-
-
-
+let object = MyClass()
+// assign : 방출된 값을 오브젝트의 어떤 값에 할당하겠다.
+let subscription3 = arrayPublisher.assign(to: \.property, on: object)
+print("Final Value: \(object.property)")
 
 //: [Next](@next)
 
