eb06d3e71541c963c0747500627df33381b46698
YuGeonHui <ghyu_0906@naver.com>
2022-06-20 08:23:06 +0900

    Subject / NotificationCenter, Timer, URLSession with Combine

--- Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,7 +4,40 @@ import Foundation
 import UIKit
 import Combine
 
+// MARK: @Published
+
+// @Published로 선언된 프로퍼티를 퍼블리셔로 만들어 줌
+// 클래스 한해서 사용이 가능하다.
+// $를 이용해서 퍼블리셔에 접근할 수 있다.
+
+final class SomeViewModel {
+    
+    @Published var name: String = "Jack"
+    var age: Int = 20
+}
+
+final class Label {
+    var text: String = ""
+}
+
+let label = Label()
+let viewModel = SomeViewModel()
+
+// ViewModel의 값을 바꿀 때마다 label의 text의 값이 변경된다.
+print("text : \(label.text)") // Empty String
+
+viewModel.$name.assign(to: \.text, on: label)
+print("text : \(label.text)") // Jack
+
+viewModel.name = "Jason"
+print("text : \(label.text)") // Jason
+
+viewModel.name = "Hoo"
+print("text : \(label.text)") // Hoo
 
 
+viewModel.age = 40
+label.text = "\(viewModel.age)"
+print("text : \(label.text)")
 
 //: [Next](@next)
--- Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,21 @@
 import Foundation
 import Combine
 
-
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[Debug]") // 관계를 보여주는 operate
+    .sink { value in
+    print("Subscriber recevied value: \(value)")
+}
+
+subject.send("Hello")
+subject.send("Hello Again!")
+subject.send("Hello for the last time!")
+//subject.send(completion: .finished) // 관계가 완료되었다.
+subscription.cancel() // 구독자가 끝는 방법
+subject.send("Hello ?? : (") // 데이터 전달되지 않는다.
 
 
 //: [Next](@next)
