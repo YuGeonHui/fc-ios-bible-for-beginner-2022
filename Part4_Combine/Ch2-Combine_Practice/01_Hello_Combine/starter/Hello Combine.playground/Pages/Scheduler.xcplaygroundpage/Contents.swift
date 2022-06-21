318ecb0e34375661a8983cf2a73bc12ba1baa2c5
YuGeonHui <ghyu_0906@naver.com>
2022-06-21 21:31:24 +0900

    Scheduler, received, Operator

--- Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Part4_Combine/Ch2-Combine_Practice/01_Hello_Combine/starter/Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -3,8 +3,19 @@
 import Foundation
 import Combine
 
+let arrPublisher = [1, 2, 3].publisher
 
+let queue = DispatchQueue(label: "custom")
 
-
+let subscription = arrPublisher
+    .subscribe(on: queue) // Heavy한 작업이라고 가정할 때
+    .map { value -> Int in
+        print("transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .receive(on: DispatchQueue.main) // 받는 값은 mainThread 
+    .sink { value in
+    print("Receive Value: \(value), thread: \(Thread.current)")
+}
 
 //: [Next](@next)
